19df2563a254514a644aae89fc7ee8303e5ed7f4
Hanna Larsen <hanna.c.larsen@gmail.com>
2017-02-03 14:28:50 -0700

    close

--- CS4150PS3/GalaxyTests/GalaxyTests.cs
+++ CS4150PS3/GalaxyTests/GalaxyTests.cs
@@ -112,6 +112,39 @@ namespace GalaxyTests
         }
 
         [TestMethod]
+        public void TestCorrect4()
+        {
+            g = new Galaxy();
+            stars = new List<Star>();
+            long galacticDiam = 0;
+            string line = "";
+            int count = 0;
+            char[] whitespace = { ' ', '\t' };
+            string[] currentLine;
+            try
+            {
+                using (StreamReader sr = File.OpenText(@"C:\Users\hannal\Documents\PS3Tests\test4.txt"))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        currentLine = line.Split(whitespace);
+                        if (count == 0)
+                        {
+                            galacticDiam = Convert.ToInt64(currentLine[0]);
+                            count++;
+                            continue;
+                        }
+                        Star s = new Star(currentLine[0], currentLine[1]);
+                        stars.Add(s);
+                    }
+                }
+            }
+            catch (Exception e)
+            { }
+            Assert.AreEqual("5", g.FindStarMajority(stars, galacticDiam));
+        }
+
+        [TestMethod]
         public void TestAllInGalaxy()
         {
             g = new Galaxy();
