a7c82d45ada94aac71bf5e49789d7b2a6e6996e8
Hanna Larsen <hanna.c.larsen@gmail.com>
2017-03-17 15:58:04 -0700

    v2

---
+++ PS8/Bank/BankQueue2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class BankQueue2
+    {
+        private List<Person> queue;
+        private int T;
+        static void Main(string[] args)
+        {
+            BankQueue2 b = new BankQueue2();
+            b.GetInfo2();
+            Console.WriteLine(b.FindMax2().ToString());
+        }
+
+        public void GetInfo2()
+        {
+            string line = "";
+            string[] currentLine;
+            int count = 0;
+            int N = 0;
+            T = 0;
+            int c = 0;
+            int t = 0;
+            Person p;
+
+            while ((line = Console.ReadLine()) != null && line != "")
+            {
+                currentLine = line.Split();
+                if (count == 0)
+                {
+                    N = Convert.ToInt32(currentLine[0]);
+                    T = Convert.ToInt32(currentLine[1]);
+                    queue = new List<Person>();
+                    count++;
+                    continue;
+                }
+
+                c = Convert.ToInt32(currentLine[0]);
+                t = Convert.ToInt32(currentLine[1]);
+
+                p = new Person(c, t);
+                queue.Add(p);
+            }
+        }
+
+        public int FindMax2()
+        {
+            int sum = 0;
+            int timeElapsed = 0;
+            queue.Sort((i1, i2) => i2.GetPrice().CompareTo(i2.GetPrice()));
+
+            foreach(Person p in queue)
+            {
+                if(p.GetTime() <= timeElapsed)
+                {
+                    sum += p.GetPrice();
+                    timeElapsed++;
+                }
+            }
+            return sum;
+        }
+    }
+
+    class Person
+    {
+        int price;
+        int time;
+        public Person(int p, int t)
+        {
+            price = p;
+            time = t;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+        public int GetTime()
+        {
+            return time;
+        }
+    }
+
+}
