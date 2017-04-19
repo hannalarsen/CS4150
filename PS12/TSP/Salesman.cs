83ea49c30a1fb45d9144a0b36a7400287e7a9bcb
Hanna Larsen <hanna.c.larsen@gmail.com>
2017-04-19 13:59:28 -0600

    not correct

--- PS12/TSP/Salesman.cs
+++ PS12/TSP/Salesman.cs
@@ -14,17 +14,18 @@ namespace TSP
         {
             Salesman s = new Salesman();
             s.GetInfo();
+            Console.WriteLine(s.MinCost());
         }
 
         public void GetInfo()
         {
             string line = "";
             string[] currentLine;
-            int count = 0;
+            int count = -1;
             while ((line = Console.ReadLine()) != null && line != "")
             {
                 currentLine = line.Split();
-                if (count == 0)
+                if (count == -1)
                 {
                     n = Convert.ToInt32(currentLine[0]);
                     matrix = new int[n,n];
@@ -33,10 +34,65 @@ namespace TSP
                 }
                 for (int i = 0; i < n; i++)
                 {
-                    matrix[count - 1, i] = Convert.ToInt32(currentLine[i]);
+                    matrix[count, i] = Convert.ToInt32(currentLine[i]);
                 }
                 count++;
             }
+        }
+
+        public int MinCost()
+        {
+            int cost = 0;
+            HashSet<int> visited = new HashSet<int>();
+            if (n == 2)
+            {
+                return matrix[0, 1] + matrix[1, 0];
+            }
+            int i = 0;       
+            int count = 0;
+            visited.Add(i);
+            while (count < n)
+            {
+                int minIndex = 0;
+                int min = 1000;
+                for (int j = 0; j < n; j++)
+                {
+                    if (count == n - 1)
+                    {
+                        cost += matrix[i, 0];
+                        count++;
+                        break;
+                    }
+                    if (j == i)
+                    {
+                        if (j == n-1)
+                        {
+                            cost += min;
+                            i = minIndex;
+                            visited.Add(minIndex);
+                            count++;
+                        }
+                        continue;
+                    }
+                    if (matrix[i, j] < min)
+                    {
+                        if (visited.Contains(j))
+                        {
+                            continue;
+                        }
+                        min = matrix[i, j];
+                        minIndex = j;
+                    }
+                    if (j == n - 1)
+                    {
+                        cost += min;
+                        i = minIndex;
+                        visited.Add(minIndex);
+                        count++;
+                    }
+                }
+            }
+            return cost;
         }
     }
 }
